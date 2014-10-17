991b46912603e551bd4e368fb608543df3ff2ddf
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-17 18:03:42 +0200

    add head plugin
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ manifests/head.pp
@@ -0,0 +1,12 @@
+class elasticsearch::head {
+
+   exec {
+      'install_head':
+	command   => "bin/plugin -install mobz/elasticsearch-head",
+        cwd       => "/usr/share/elasticsearch",
+        path      => "/bin/:/usr/bin/:/sbin/:/usr/sbin",
+        unless    => "ls /usr/share/elasticsearch/plugins/head 2>/dev/null", 
+   }
+
+
+}
