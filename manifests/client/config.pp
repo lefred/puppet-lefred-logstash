058029118183c5a1d0d3ce86313557f2551bfcdd
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-24 18:10:20 +0200

    remove duplicate declaration
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -5,7 +5,8 @@ class logstash::client::config {
         ensure  => directory;
     "/etc/logstash-forwarder/conf":
         ensure  => directory,
-        require => File['/etc/logstash-forwarder'];
+        require => File['/etc/logstash-forwarder'],
+        ensure  => "directory";
     "/etc/logstash-forwarder/forwarder.crt":
         ensure  => present,
         require => File['/etc/logstash-forwarder'],
@@ -14,9 +15,6 @@ class logstash::client::config {
         ensure  => present,
         require => File['/etc/logstash-forwarder'],
         source  => "puppet:///modules/logstash/forwarder.key";
-    "/etc/logstash-forwarder/conf/":
-        require => File['/etc/logstash-forwarder'],
-        ensure  => "directory";
   }
   
 }  
