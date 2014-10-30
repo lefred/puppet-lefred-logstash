c8bc31eb7b99de221b17eba98c8455c8167fe3f6
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-30 17:55:29 +0100

    no luck
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,6 +3,4 @@ class logstash::client {
   include logstash::client::config
   include logstash::client::packages
   
-  Class['logstash::client::packages'] -> Class['logstash::client::config']
-
 }
