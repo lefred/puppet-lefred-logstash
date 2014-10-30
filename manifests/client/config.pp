43dc09e333528ec4c071fa58bd61a7a046fcf66b
Frederic Descamps <lefred.descamps@gmail.com>
2014-10-30 17:31:03 +0100

    add config
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -1,11 +1,17 @@
 class logstash::client::config {
-  
+ 
+ $logstash_server = $logstash::client::server
+ 
  file {
     "/etc/logstash-forwarder/":
         ensure  => directory;
     "/etc/logstash-forwarder/conf/":
         require => File['/etc/logstash-forwarder'],
         ensure  => "directory";
+    "/etc/logstash-forwarder/conf/config.json":
+	require => File['/etc/logstash-forwarder/conf/'],
+	ensure  => present,
+	content => template("conf/config.json.erb");
     "/etc/logstash-forwarder/forwarder.crt":
         ensure  => present,
         require => File['/etc/logstash-forwarder'],
