2db0498bbeda25f1e71e0b92268ead68c3beba6f
Frederic Descamps <lefred.descamps@gmail.com>
2014-12-04 23:00:02 +0100

    remove dep
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,11 +4,9 @@ class logstash::config {
  file {
     "/etc/logstash/forwarder.crt":
         ensure  => present,
-        require => File['/etc/logstash-forwarder'],
         source  => "puppet:///modules/logstash/forwarder.crt";
     "/etc/logstash/forwarder.key":
         ensure  => present,
-        require => File['/etc/logstash-forwarder'],
         source  => "puppet:///modules/logstash/forwarder.key";
   }
   
