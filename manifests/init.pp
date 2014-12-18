2e00897f828b07413f347e481f4e9cb8288a9ea9
Frederic Descamps <lefred.descamps@gmail.com>
2014-12-18 10:33:47 +0100

    add service
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,8 @@ class logstash ($version="1.4") {
   include logstash::repository
   include logstash::packages
   include logstash::config
+  include logstash::service
   
-  Class['logstash::repository'] -> Class['logstash::packages'] -> Class['logstash::config']
+  Class['logstash::repository'] -> Class['logstash::packages'] -> Class['logstash::config'] -> Class['logstash::service']
  
 }
